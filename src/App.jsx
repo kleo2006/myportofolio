0657921b19a2e80e85bc7cad3fcced5ed6fb63c8
Kleo Fili <kleofili60@gmail.com>
2026-07-15 15:00:28 +0200

    update design

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Projects from './components/Projects/Projects';
 import Experience from './components/Experience/Experience';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
+import CinematicBackground from './components/CinematicBackground/CinematicBackground';
 
 function App() {
   return (
     <>
+      <CinematicBackground />
       <Navbar />
       <main>
         <Hero />
