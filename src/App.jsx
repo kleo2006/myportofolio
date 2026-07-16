4fca52d7aec220672c4d2d73b81d2af1e5c8fce8
Kleo Fili <kleofili60@gmail.com>
2026-07-16 13:48:41 +0200

    fix faq

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
-import FAQ from './components/FAQ/Faq';
+import FAQ from './components/Faq/Faq';
 import About from './components/About/About';
 import Certificate from './components/Certificate/Certificate';
 import Skills from './components/Skills/Skills';
