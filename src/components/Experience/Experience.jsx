3dbcc2f324e64009f3dee99c0ebf2f9a66b0db83
Kleo Fili <kleofili60@gmail.com>
2026-07-16 13:56:37 +0200

    fix imports

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,7 +1,7 @@
 import { memo } from 'react';
 import { EXPERIENCE } from '../../data/experience';
 import { useScrollReveal } from '../../hooks/useScrollReveal';
-import GithubActivity from '../GithubActivity/GithubActivity';
+import GithubActivity from '../GitHubActivity/GithubActivity';
 import './Experience.css';
 
 function TimelineItem({ entry, index, isLast }) {
